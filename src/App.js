42c3647c05defe7c19d35db02387e7bd5c1f5cea
koreanleo <dhrn0623@naver.com>
2022-06-29 18:24:49 +0900

    add movie

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import {useState,useEffect} from "react";
-
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom"
 
 function App() {
-  const [loading,setLoading] =useState(true); 
-  const [movies,setMovies] =useState([]);
-  const getMovies = async()=>{
-    const json = await (
-        await (fetch(
-          "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-        )
-      )).json();
-    setMovies(json.data);
-    setLoading(false);
-  }
-  useEffect(()=>{
-    getMovies();
-  },[]);
-  console.log(movies);
   return (
-    <div>
-      {loading? <h1>Loading....</h1>:null}
-    </div>
-  ); 
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Routes>
+          <Route path="/movie/:id" element={<Detail />} />
+        </Routes>
+      </Router>
+    );
 }
 
 export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,26 @@
+import proTypes from "prop-types";
+import {Link} from "react-router-dom"
+
+function Movie({id, coverImg, title, summary, genres}){
+    return <div >
+    <img alt="img" src={coverImg}/>
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
+    <p>{summary}</p>
+    <ul>
+    {genres.map((g)=>(
+      <li key={g}>{g}</li>
+    ))}
+    </ul>
+  </div>;
+}
+
+Movie.proTypes={
+    id:proTypes.number.isRequired,
+    coverImg:proTypes.string.isRequired,
+    title:proTypes.string.isRequired,
+    summary:proTypes.string.isRequired,
+    genres:proTypes.arrayOf(proTypes.string).isRequired,
+}
+export default Movie;
